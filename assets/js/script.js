54202b2ff6c1e8ad83643a844d388474ec3d958e
“Ivan” <vlasoff.ivan95@gmail.com>
2022-09-07 01:03:30 +0300

    add all task

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,7 +15,7 @@ const isAdult = function (age) {
 const checkMultiplicity = function (var1, var2) {
     if (!isNaN(var1) && !isNaN(var2)) {
         return (var1 % var2 === 0);
-    }
+    };
     return null;
 };
 
@@ -27,10 +27,36 @@ const checkMultiplicity = function (var1, var2) {
 
 const checkTriangle = function (a, b, c) {
     if (!isNaN(a) && !isNaN(b) && !isNaN(c)) {
-
         return (a + b > c && a + c > b && b + c > a);
     };
     return null;
 };
+
+//const var1 = prompt('Enter var: ');
+
+const sumOdd = function (var1) {
+    let result = 0;
+    for (i = 1; i <= var1; i++) {
+        result += i;
+        i += 1;
+    };
+    return result;
+};
+
+
+//const var1 = prompt('Enter variable: ');
+
+const checkPrime = function (var1) {
+    if (!isNaN(var1)) {
+        for (let i = 2; i < var1; i++) {
+            if (var1 % i === 0) {
+                return false;
+            };
+            return true;
+        };
+    };
+    return null;
+};
+
 
 
